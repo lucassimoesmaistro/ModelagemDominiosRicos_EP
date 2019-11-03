b8daeb12a265e7ef6ad55a6565de05bbf1c85b22
Lucas Simões <lucassimoes_@hotmail.com>
2019-11-03 09:26:06 -0300

    Final Aula - Integração de BC's - Processamento do pedido

--- src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace NerdStore.Vendas.Application.Events
     public class PedidoEventHandler :
         INotificationHandler<PedidoRascunhoIniciadoEvent>,
         INotificationHandler<PedidoAtualizadoEvent>,
-        INotificationHandler<PedidoItemAdicionadoEvent>
+        INotificationHandler<PedidoItemAdicionadoEvent>,
+        INotificationHandler<PedidoEstoqueRejeitadoEvent>
     {
 
         //private readonly IMediatorHandler _mediatorHandler;
@@ -28,6 +30,11 @@ namespace NerdStore.Vendas.Application.Events
         }
 
         public Task Handle(PedidoItemAdicionadoEvent notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(PedidoEstoqueRejeitadoEvent notification, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
